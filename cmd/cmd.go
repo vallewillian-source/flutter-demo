4e178601de0d588105009075e6e6d024c0de424c
Willian Valle <willian@pinkapp.com>
2022-05-13 23:13:17 -0300

    cmd package skeleton

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,21 +1,5 @@
 package cmd
 
-import (
-	"time"
-)
-
-type user struct {
-	guid          int
-	name          string `json:"name"`
-	username      string
-	telephone     string
-	password_hash string    `json:"password_hash"`
-	token_hash    string    `json:"token_hash"`
-	creation_ts   time.Time `json:"creation_ts"`
-	upgrade_ts    time.Time `json:"upgrade_ts"`
-	deactivated   bool
-}
-
 func Execute() {
 	print("OK")
 }
